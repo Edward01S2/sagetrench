d085bd41c3bed42ab308146876d5260b54d593fa
edward01s2 <edward.01s2@gmail.com>
2020-06-01 11:08:57 -0500

    Fix fonts

--- app/setup.php
+++ app/setup.php
@@ -11,6 +11,7 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'google_fonts', '//fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,700&display=swap', false, null );
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
