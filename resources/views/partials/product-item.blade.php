82e58e37e3d4fd7011d612174489e0dd48dd6afd
edward01s2 <edward.01s2@gmail.com>
2020-06-03 10:53:32 -0500

    Fix products

--- resources/views/partials/product-item.blade.php
+++ resources/views/partials/product-item.blade.php
@@ -2,7 +2,7 @@
   <a class="" href="/product/#{!! $cat->slug !!}">
     <div class="group">
       <img class="z-0 relative group-hover:opacity-75 object-cover object-center h-48 w-full lg:h-64" src="{!! $image["url"] !!}" alt=""/>
-      <a class="block px-8 lg:px-12" href="/product/#{!! $cat->slug !!}">
+      <a class="block px-8 lg:px-12" href="/products/#{!! $cat->slug !!}">
         <div class="text-center bg-s-dark text-white uppercase px-4 py-2 tracking-widest font-medium -mt-4 z-10 relative group-hover:bg-s-stone group-hover:text-s-yellow">
           {!! $cat->name !!}
         </div>
